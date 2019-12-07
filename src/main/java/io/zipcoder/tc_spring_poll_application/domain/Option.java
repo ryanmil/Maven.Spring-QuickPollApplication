47544200c3ba3a9d21e43db03dcdc165a4b10325
Ryan <ryan@rmiller.dev>
2019-12-07 13:40:59 -0500

    hoi

---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/domain/Option.java
@@ -0,0 +1,33 @@
+package io.zipcoder.tc_spring_poll_application.domain;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
+public class Option {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "OPTION_ID")
+    private Long id;
+
+    private String option;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getOption() {
+        return option;
+    }
+
+    public void setOption(String option) {
+        this.option = option;
+    }
+}
---
+++ src/main/java/io/zipcoder/tc_spring_poll_application/repositories/VoteRepository.java
@@ -0,0 +1,12 @@
+package io.zipcoder.tc_spring_poll_application.repositories;
+
+import io.zipcoder.tc_spring_poll_application.domain.Vote;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface VoteRepository extends CrudRepository<Vote, Long> {
+
+
+
+}
